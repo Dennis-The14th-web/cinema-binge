113d7ead0a21a2f51a88b191d69f6a3b596dd141
the 14th web <dennis.itua1@gmail.com>
2020-09-05 19:02:41 -0400

    updated app modal footer result

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         return (
             <div className="footer fixed-bottom ">
             <div>
-                <a className="fa fa-github" href="https://github.com/Dennis-The14th-web"></a>
+                <a className="fa fa-github" href="https://github.com/Dennis-The14th-web/cinema-binge"></a>
                 <p><small><b>&#9400; 2020 Dennis Itua </b></small></p> 
                 </div>            
     </div>
